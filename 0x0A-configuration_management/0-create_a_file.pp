f422a808ed7e81f3f161403cf7386e3e2f00142f
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-12 12:02:25 -0700

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # creates file in /tmp
 
-file { 'tmp/school':
+file { 'school':
     ensure  => 'present',
+    path    => '/tmp/school',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
