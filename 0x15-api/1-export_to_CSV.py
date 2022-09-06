58f610e867d6bc5d7ba7a1ef34301ad0265eb44a
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-09-06 14:39:59 -0700

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """task 1"""
+
 import csv
 import requests
 from sys import argv
 
-api = 'https://jsonplaceholder.typicode.com/'
-employee = requests.get(api + "users/{}".format(argv[1])).json()
-employee_username = employee['username']
+if __name__ == "__main__":
+    api = 'https://jsonplaceholder.typicode.com/'
+    employee = requests.get(api + "users/{}".format(argv[1])).json()
+    employee_username = employee['username']
 
-tasks = requests.get(api + "todos/", params={'userId': argv[1]}).json()
+    tasks = requests.get(api + "todos/", params={'userId': argv[1]}).json()
 
-filename = argv[1] + ".cvs"
-with open(filename, 'w') as f:
-    writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL)
-    for task in tasks:
-        writer.writerow([argv[1], employee_username, task['completed'], task['title']])
+    filename = argv[1] + ".cvs"
+    with open(filename, 'w') as f:
+        writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writer.writerow([argv[1], employee_username,
+                            task['completed'], task['title']])
