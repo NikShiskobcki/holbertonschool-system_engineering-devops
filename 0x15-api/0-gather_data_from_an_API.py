db885e42a14991da39275cf9a8e42924638f3897
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-09-06 14:27:45 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,21 +3,22 @@
 import requests
 from sys import argv
 
-api = 'https://jsonplaceholder.typicode.com/'
-employee = requests.get(api + "users/{}".format(argv[1])).json()
-employee_name = employee['name']
+if __name__ == '__main__':
+    api = 'https://jsonplaceholder.typicode.com/'
+    employee = requests.get(api + "users/{}".format(argv[1])).json()
+    employee_name = employee['name']
 
-tasks = requests.get(api + "todos/", params={'userId': argv[1]}).json()
+    tasks = requests.get(api + "todos/", params={'userId': argv[1]}).json()
 
-taskCount = len(tasks)
-tasksDone = 0
+    taskCount = len(tasks)
+    tasksDone = 0
 
-for task in tasks:
+    for task in tasks:
     if task['completed'] is True:
         tasksDone += 1
 
-print("Employee {} is done with tasks({}/{})".format(employee_name,
+    print("Employee {} is done with tasks({}/{})".format(employee_name,
                                                      tasksDone, taskCount))
-for task in tasks:
-    if task['completed'] is True:
-        print("\t " + task['title'])
+    for task in tasks:
+        if task['completed'] is True:
+            print("\t " + task['title'])
