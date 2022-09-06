b0f140edc32540cd107515fb10f73b0f9fc8b4d6
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-09-06 14:57:09 -0700

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""task 2"""
+
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    api = 'https://jsonplaceholder.typicode.com/'
+    employee = requests.get(api + "users/{}".format(argv[1])).json()
+    employee_username = employee['username']
+
+    tasks = requests.get(api + "todos/", params={'userId': argv[1]}).json()
+
+    filename = argv[1] + ".json"
+
+    lst = []
+    for task in tasks:
+        dictAux = {}
+        dictAux["task"] = item.get('title')
+        dictAux["completed"] = item.get('completed')
+        dictAux["username"] = employee_username
+        lst.append(dictAux)
+    aux = {}
+    aux[argv[1]] = lst
+    with open(filename, 'w') as f:
+        json.dump(aux, f)
